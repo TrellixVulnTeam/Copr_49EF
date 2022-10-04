037a8ad7e2626bd1662da642ab993fa206472591
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 19:38:57 +0000

    Adding numeric_owner as keyword arguement

--- backend/tests/test_action.py
+++ backend/tests/test_action.py
@@ -109,7 +109,7 @@ class TestAction(object):
                     if not is_within_directory(path, member_path):
                         raise Exception("Attempted Path Traversal in Tar File")
             
-                tar.extractall(path, members, numeric_owner) 
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
                 
             
             safe_extract(tfile, os.path.join(self.tmp_dir_name,"old_dir"))
